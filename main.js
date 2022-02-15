129203230b57b4151f2414e70047e576debdd510
Faiyaz Husain <66625103+Vulcan-F22175@users.noreply.github.com>
2022-02-16 01:44:35 +0500

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 client.once('ready', () => {
-    console.log('Raiden Is Online...')
+    console.log('Bot Is Online...')
 });
 // Add Your Bot token between the two apostrophes below
 client.login('');
